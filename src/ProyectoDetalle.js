c37522b7a8323d9fb156cf0ea9aa31519d40e3b5
martinlopez1234 <martin.lr@hotmail.com>
2024-01-22 18:51:25 -0300

    agregando nuevos requerimientos

--- src/ProyectoDetalle.js
+++ src/ProyectoDetalle.js
@@ -116,10 +116,10 @@ const ProyectoDetalle = () => {
       </div>
       <div class="container-fluid mt-5">
         <div class="row">
-          <div class="col-md-8 order-md-1 order-2">
+          <div class="col-md-8 order-md-1 order-1">
             <img src="https://images.unsplash.com/photo-1556156657-b2925b706f19?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="img-fluid" alt="Imagen" />
           </div>
-          <div class="col-md-4 order-md-2 order-1">
+          <div class="col-md-4 order-md-2 order-2">
             <h5>
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat rem ea minus consectetur. Necessitatibus deserunt amet eius quis vel, cumque aspernatur praesentium, officiis molestiae esse repellendus pariatur qui expedita! Voluptate.
             </h5>
